282ff4aff3c8c258b6bec0cfff7fcdfa6f15e76f
Andrew <63925540+An-drew-bit@users.noreply.github.com>
2022-02-18 16:52:58 +0300

    update_project

--- index.php
+++ index.php
@@ -4,13 +4,11 @@ use factories\{DBFactory, MysqlFactory, PostgreFactory, OracleFactory};
 
 function createDB(DbFactory $factory)
 {
-
     $connection = $factory->createConnection();
     $recrord = $factory->createRecrord();
     $queryBuiler = $factory->createQueryBuiler();
 
     echo $connection->useConnection() . "\n" . $recrord->useRecrord(). "\n" .$queryBuiler->useQueryBuiler()."<br>";
-
 }
 
 createDB(new MysqlFactory());
